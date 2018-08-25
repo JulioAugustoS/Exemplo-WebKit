875698766d56c117d56c5cc552e22038ece8c42e
Usuário Convidado <Guest@L2602MICRO12.local>
2018-08-25 11:38:48 -0300

    Projeto 5

--- Exemplo WebKit/ViewController.swift
+++ Exemplo WebKit/ViewController.swift
@@ -7,11 +7,32 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
+    @IBOutlet weak var meuWebKit: WKWebView!
+    @IBOutlet weak var meuActivity: UIActivityIndicatorView!
+    
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        meuActivity.stopAnimating()
+        meuActivity.isHidden = true
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        meuWebKit.navigationDelegate = self
+        
+        meuActivity.startAnimating()
+        
+        let url = URL(string: "http://www.vive.com/us")
+        let request = URLRequest(url: url!)
+        meuWebKit.load(request)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
